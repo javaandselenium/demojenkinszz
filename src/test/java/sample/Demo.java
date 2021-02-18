bfd914e0f8971ef7e0f418b5df1c1f12d23fb6aa
QSP <QSP@DESKTOP-EVK9GMC>
2021-02-18 07:10:05 +0530

    Sample

---
+++ src/test/java/sample/Demo.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void test() {
+		Reporter.log("Hello selenium",true);
+	}
+
+}
